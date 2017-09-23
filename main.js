69f6e7412948bb3c3028beb141e772ff0e536c2d
nataliefrancis <natdfrancis@gmail.com>
2017-09-23 16:39:28 -0600

    includes a winstate

--- main.js
+++ main.js
@@ -6,21 +6,30 @@ let horseTwo = document.getElementById('blackHorse');
 let twoMove = 0;
 
 
-document.addEventListener("keydown", function(event) {
-	switch (event.key) {
-		case "Escape" :
+document.addEventListener("keydown", function(event) { //set event listener
+	switch (event.key) { 
+		case "Escape" :                                //Player one uses escape key
 			console.log("Brown horse moves");
-			oneMove += 5;
-			horseOne.style.paddingLeft = oneMove + "%";	
+			oneMove += 5;								//every click adds to their move counter
+			horseOne.style.left = oneMove + "%";		//for every click it'll move a % to the right
+			checkWin("Brown horse");	
 			break;
 
-		case "ArrowRight" :
+		case "ArrowRight" :                             //Player two uses right arrow
 			console.log("Black horse moves");
 			twoMove += 5;
-			horseTwo.style.paddingLeft = twoMove + "%";
+			horseTwo.style.left = twoMove + "%";
+			checkWin("Black horse");
 			break;
+	}
+
+	function checkWin(name) {					//once a player's move counter reaches 90, they win
+		if (oneMove === 90) {
+			alert(name + " wins!");
+		} else if (twoMove === 90) {
+			alert(name + " wins!");
+		}
 	}
 });
 
 
-//So now I have it "moving" but not really. Still working on it//
